3ee4e76e6f3d18738a6e27deca22e56512eed6a5
jonyIsgood <jonytoogood@gmail.com>
2025-08-25 10:41:05 +0300

    change endpoints

--- config/endpoints.php
+++ config/endpoints.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'prefix_auth' => 'http://arch.homework/otusapp/aemelyanenko/api/',
+    'prefix_auth' => 'http://arch.homework/app/insurance/api/',
     'prefix' => 'auth/v1/',
     'auth-service' => 'auth/v1/',
 
-    'prefix_policy' => 'http://arch.homework/otusapp/aemelyanenko/api/',
+    'prefix_policy' => 'http://arch.homework/app/insurance/api/',
     'prefix_policy_v1' => 'policy/v1/',
     'policy-service' => 'policy/v1/',
 
-    'prefix_billing' => 'http://arch.homework/otusapp/aemelyanenko/api/',
+    'prefix_billing' => 'http://arch.homework/app/insurance/api/',
     'prefix_billing_v1' => 'billing/v1/',
     'billing-service' => 'billing/v1/',
 
-    'prefix_storage' => 'http://arch.homework/otusapp/aemelyanenko/api/',
+    'prefix_storage' => 'http://arch.homework/app/insurance/api/',
     'prefix_storage_v1' => 'storage/v1/',
     'storage-service' => 'storage/v1/'
 ];
